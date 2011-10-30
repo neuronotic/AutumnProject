cf1092e6146f95753a548a168d762310345e85f7
neuronotic <dm0724@bris.ac.uk>
2011-10-30 16:33:51 +0000

    added support for multiple segments in ItineraryImpl

--- Sratch/src/main/java/traffic/ItineraryImpl.java
+++ Sratch/src/main/java/traffic/ItineraryImpl.java
@@ -1,34 +1,45 @@
 package traffic;
 
+import static java.util.Arrays.*;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class ItineraryImpl implements Itinerary {
-	private final Segment route;
+	private final List<Segment> segments;
 
-	public ItineraryImpl(final Segment route) {
-		this.route = route;
+	public ItineraryImpl(final Segment...segments) {
+		this(asList(segments));
+	}
+
+	public ItineraryImpl(final List<Segment> segments) {
+		this.segments = segments;
 	}
 
 	@Override
 	public Segment route() {
-		return route;
+		return segments.get(0);
 	}
 
 	@Override
 	public Iterator<Cell> iterator() {
 		final List<Cell> result = new ArrayList<Cell>();
-		result.add(route.inJunction());
-		for (final Cell cell : route.cellChain()) {
-			result.add(cell);
+		for (final Iterator<Segment> iterator = segments.iterator(); iterator.hasNext();) {
+			final Segment segment = iterator.next();
+			result.add(segment.inJunction());
+			for (final Cell cell : segment.cellChain()) {
+				result.add(cell);
+			}
+			if (!iterator.hasNext()) {
+				result.add(segment.outJunction());
+			}
 		}
-		result.add(route.outJunction());
 		return result.iterator();
 	}
 
 	@Override
 	public String toString() {
-		return String.format("Itinerary %s", route);
+		return String.format("Itinerary %s", segments);
 	}
 }
--- Sratch/src/test/java/traffic/TestItineraryImpl.java
+++ Sratch/src/test/java/traffic/TestItineraryImpl.java
@@ -3,32 +3,65 @@ package traffic;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jmock.Expectations;
 import org.junit.Rule;
 import org.junit.Test;
+
+import com.google.common.collect.Iterators;
 
 public class TestItineraryImpl {
 	@Rule
 	public final JUnitRuleMockery context = new JUnitRuleMockery();
 
-	private final Segment segment = context.mock(Segment.class);
-	private final CellChain cellChain = context.mock(CellChain.class);
-	private final Junction inJunctionCell = context.mock(Junction.class, "inJunctionCell");
+	private final Segment segment0 = context.mock(Segment.class, "segment0");
+	private final Segment segment1 = context.mock(Segment.class, "segment1");
+
+	private final CellChain cellChain0 = context.mock(CellChain.class, "cellChain0");
+	private final CellChain cellChain1 = context.mock(CellChain.class, "cellChain1");
+
+	private final Junction junctionCell0 = context.mock(Junction.class, "junctionCell0");
+	private final Junction junctionCell1 = context.mock(Junction.class, "junctionCell1");
+	private final Junction junctionCell2 = context.mock(Junction.class, "junctionCell2");
+
+	private final Cell segmentCell0 = context.mock(Cell.class, "segmentCell0");
 	private final Cell segmentCell1 = context.mock(Cell.class, "segmentCell1");
 	private final Cell segmentCell2 = context.mock(Cell.class, "segmentCell2");
-	private final Junction outJunctionCell = context.mock(Junction.class, "outJunctionCell");
+
+	@Test
+	public void itineraryContainsCellsFromRouteJunctionsAndSegmentsInOrderForMultipleSegments() throws Exception {
+		context.checking(new Expectations() {
+			{
+				oneOf(segment0).inJunction(); will(returnValue(junctionCell0));
+				oneOf(segment0).cellChain(); will(returnValue(cellChain0));
+				oneOf(cellChain0).iterator(); will(returnIterator(segmentCell0, segmentCell1));
+
+				oneOf(segment1).inJunction(); will(returnValue(junctionCell1));
+				oneOf(segment1).cellChain(); will(returnValue(cellChain1));
+				oneOf(cellChain1).iterator(); will(returnIterator(segmentCell2));
+				oneOf(segment1).outJunction(); will(returnValue(junctionCell2));
+			}
+		});
+
+		final ItineraryImpl itineraryImpl = new ItineraryImpl(segment0, segment1);
+		final List<Cell> cells = new ArrayList<Cell>();
+		Iterators.addAll(cells, itineraryImpl.iterator());
+		assertThat(cells, contains(junctionCell0, segmentCell0, segmentCell1, junctionCell1, segmentCell2, junctionCell2));
+	}
 
 	@Test
 	public void itineraryContainsCellsFromRouteJunctionsAndSegmentsInOrder() throws Exception {
 		context.checking(new Expectations() {
 			{
-				oneOf(segment).inJunction(); will(returnValue(inJunctionCell));
-				oneOf(segment).cellChain(); will(returnValue(cellChain));
-				oneOf(cellChain).iterator(); will(returnIterator(segmentCell1, segmentCell2));
-				oneOf(segment).outJunction(); will(returnValue(outJunctionCell));
+				oneOf(segment0).inJunction(); will(returnValue(junctionCell0));
+				oneOf(segment0).cellChain(); will(returnValue(cellChain0));
+				oneOf(cellChain0).iterator(); will(returnIterator(segmentCell1, segmentCell2));
+				oneOf(segment0).outJunction(); will(returnValue(junctionCell1));
 			}
 		});
 
-		assertThat(new ItineraryImpl(segment), contains(inJunctionCell, segmentCell1, segmentCell2, outJunctionCell));
+		assertThat(new ItineraryImpl(segment0), contains(junctionCell0, segmentCell1, segmentCell2, junctionCell1));
 	}
 }
