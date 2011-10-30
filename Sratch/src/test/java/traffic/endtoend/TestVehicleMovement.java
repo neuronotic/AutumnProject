7c267a24a048bb9e777f8844a33b68f53d8fe48b
neuronotic <dm0724@bris.ac.uk>
2011-10-30 14:39:21 +0000

    Added end2end test for handling a road trip across a network of multiple segments.

--- Sratch/src/test/java/traffic/endtoend/TestVehicleMovement.java
+++ Sratch/src/test/java/traffic/endtoend/TestVehicleMovement.java
@@ -23,6 +23,31 @@ public class TestVehicleMovement {
 	private final VehicleFactory vehicleFactory = new VehicleFactoryImpl();
 
 	@Test
+	public void tripAcrossTwoSegmentNetworkWithLengths4And3Takes10Timesteps() throws Exception {
+		final Junction junction0 = junction("junction0");
+		final Junction junction1 = junction("junction1");
+		final Junction junction2 = junction("junction2");
+
+		final Segment segment0 = segment(junction0, cellChainOfLength(5), junction1);
+		final Segment segment1 = segment(junction1, cellChainOfLength(5), junction2);
+
+		final RoadNetwork roadNetwork = roadNetwork(segment0, segment1);
+
+		final Trip trip = tripFrom(junction0).to(junction1);
+
+		final Itinerary itinerary = planItineraryForTrip(trip, roadNetwork);
+
+		final VehicleManager vehicleManager = vehicleManager();
+		final Vehicle vehicle = vehicleFactory.createVehicle(itinerary);
+
+		vehicleManager.addVehicle(vehicle);
+		vehicleManager.step(10);
+
+		assertThat(vehicle, isLocatedAt(junction2));
+		assertThat(vehicle, hasJourneyTime(10));
+	}
+
+	@Test
 	public void tripAcrossSingleSegmentNetworkOfLength5Takes7Timesteps() {
 		final Junction junction0 = junction("junction0");
 		final Junction junction1 = junction("junction1");
