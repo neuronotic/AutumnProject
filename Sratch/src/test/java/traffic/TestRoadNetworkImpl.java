95a9f3076e72ef29132d66be0d3dc9915f2e16c2
neuronotic <dm0724@bris.ac.uk>
2011-11-02 20:28:39 +0000

    forgot file from last commit.

--- Sratch/src/test/java/traffic/TestRoadNetworkImpl.java
+++ Sratch/src/test/java/traffic/TestRoadNetworkImpl.java
@@ -3,6 +3,7 @@ package traffic;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import org.jmock.Expectations;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -11,9 +12,20 @@ public class TestRoadNetworkImpl {
 	public final JUnitRuleMockery context = new JUnitRuleMockery();
 
 	private final Segment segment = context.mock(Segment.class);
-	private final Junction origin = context.mock(Junction.class, "origin");
-	private final Junction destination = context.mock(Junction.class, "destination");
-	private final Trip trip = context.mock(Trip.class);
+	private final Junction junction0 = context.mock(Junction.class, "junction0");
+	private final Junction junction1 = context.mock(Junction.class, "junction1");
+
+	@Test
+	public void junctionsReturnsListOfJunctionsOnNetwork() throws Exception {
+		context.checking(new Expectations() {
+			{
+				oneOf(segment).inJunction(); will(returnValue(junction0));
+				oneOf(segment).outJunction(); will(returnValue(junction1));
+			}
+		});
+
+		assertThat(new RoadNetworkImpl(segment).junctions(), containsInAnyOrder(junction0, junction1));
+	}
 
 	@Test
 	public void segmentsReturnsListOfSegmentsOnNetwork() throws Exception {
