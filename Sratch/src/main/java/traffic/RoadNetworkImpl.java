04e9a740ec6f3c341df0d08f32f0d81fe7f1eec8
neuronotic <dm0724@bris.ac.uk>
2011-11-11 11:43:53 +0000

    refactoring extracted method for building set of junctions and store set of junctions with object

--- Sratch/src/main/java/traffic/RoadNetworkImpl.java
+++ Sratch/src/main/java/traffic/RoadNetworkImpl.java
@@ -5,16 +5,19 @@ import static java.util.Arrays.*;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
 public class RoadNetworkImpl implements RoadNetwork {
 	private final List<Segment> segments;
+	private final Set<Junction> junctions;
 
 	@Inject public RoadNetworkImpl(
 			@Assisted final List<Segment> segments) {
 		this.segments = segments;
+		junctions = buildSetOfJunctions();
 	}
 
 	public RoadNetworkImpl(final Segment...segments) {
@@ -28,7 +31,11 @@ public class RoadNetworkImpl implements RoadNetwork {
 
 	@Override
 	public Collection<Junction> junctions() {
-		final Collection<Junction> junctions = new HashSet<Junction>();
+		return junctions();
+	}
+
+	private Set<Junction> buildSetOfJunctions() {
+		final Set<Junction> junctions = new HashSet<Junction>();
 		for (final Segment segment : segments) {
 			junctions.add(segment.inJunction());
 			junctions.add(segment.outJunction());
